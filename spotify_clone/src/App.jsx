c6b3f001cca101555e4c3c930c6b770b04e5d14b
elijah571 <elijahfx43@gmail.com>
2024-08-17 19:33:27 +0100

    sidebar

--- spotify_clone/src/App.jsx
+++ spotify_clone/src/App.jsx
@@ -1,7 +1,12 @@
-
-function App() {
+import React from 'react'
+import Sidebar from './components/Sidebar'
+const App = () => {
   return (
-    <div></div>
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+      <Sidebar/>
+      </div>
+    </div>
   )
 }
 
