9e1eea59195ca33bd007066c475ebceb439c47e8
elijah571 <elijahfx43@gmail.com>
2024-08-19 18:27:45 +0100

    spotify

--- spotify_clone/src/App.jsx
+++ spotify_clone/src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
-import Player from './components/Player'
+import React, {useContext} from 'react';
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import Display from './components/Display';
+import { PlayerContext } from './components/PlayerContext';
+
 const App = () => {
+  const { audioRef, track } = useContext(PlayerContext);
+
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
-      <Sidebar/>
+        <Sidebar />
+        <Display />
       </div>
-      <Player/>
+      <Player />
+      <audio ref={audioRef} src={track.file} preload='autos'></audio>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
